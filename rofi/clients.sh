ed6582111d7894cd9af92c514fa426b969511d5c
Razigli <ilgizarhisametdinov40@gmail.com>
2026-08-22 12:16:31 +0500

    fix errors

--- rofi/clients.sh
+++ rofi/clients.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
 if [[ "$ROFI_RETV" == "0" ]]; then
-	swaymsg -t get_tree | jq -c '.. | select(.type? == "con" and .name != null) | {"id": .id, "name": "\(.app_id): \(.name)"}' | while read -r client; do
+	swaymsg -t get_tree | jq -c '.. | select(.type? == "con" or .type? == "floating_con") | {"id": .id, app_id: .app_id, "name":  .name}' | while read -r client; do
 		name="$(echo "$client" | jq -r '.name')"
-		con_id="$(echo "$client" | jq -r '.id')"
-		echo -en "$name\0info\x1f$con_id\n"
+		if [[ "$name" == "null" ]]; then
+			continue
+		fi
+		app_id="$(echo "$client" | jq -r '.app_id')"
+		client_id="$(echo "$client" | jq -r '.id')"
+		echo -en "$app_id: $name\0info\x1f$client_id\n"
 	done
 else
 	swaymsg '[con_id='$ROFI_INFO'] focus' >> /dev/null
